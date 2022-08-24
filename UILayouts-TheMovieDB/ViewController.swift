d3cf1a20a8bc9e51514cf59f58e355dfa636f40d
Mwai Banda <bandamwai@gmail.com>
2022-08-24 16:24:46 -0500

    Initial Commit

---
+++ UILayouts-TheMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILayouts-TheMovieDB
+//
+//  Created by Mwai Banda on 8/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
